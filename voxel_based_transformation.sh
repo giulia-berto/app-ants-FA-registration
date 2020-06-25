edf8b7927ee6029980d38da79a3fb358dd7e698a
gberto <gberto@fbk.eu>
2020-06-25 03:21:13 -0400

    update

--- voxel_based_transformation.sh
+++ voxel_based_transformation.sh
@@ -2,28 +2,34 @@
 
 
 FA_MOVE=$1
-FA_STAT=$2
-
-FA_ANT_PRE=sub-${SUB_MOVE}_space_${SUB_STAT}_var-fa_
-
+template=FMRIB58_FA_1mm.nii.gz
 
 #------------------------------------------------------------------------------
 # ANTS Transformation of structural images
 #------------------------------------------------------------------------------
 
 # Warp Computation
-ANTS 3 -m CC[${FA_STAT},${FA_MOVE},1,5] -t SyN[0.5] \
-    -r Gauss[2,0] -o $FA_ANT_PRE -i 30x90x20 --use-Histogram-Matching
+ANTS 3 -m CC[${template},${FA_MOVE},1,5] -t SyN[0.5] \
+    -r Gauss[2,0] -o -i 30x90x20 --use-Histogram-Matching
 
 #------------------------------------------------------------------------------
 # Renaming files
 #------------------------------------------------------------------------------
 
-cp ${FA_ANT_PRE}Warp.nii.gz var-fa_warp.nii.gz
-cp ${FA_ANT_PRE}InverseWarp.nii.gz var-fa_inverse-warp.nii.gz
+#cp ${FA_ANT_PRE}Warp.nii.gz var-fa_warp.nii.gz
+#cp ${FA_ANT_PRE}InverseWarp.nii.gz var-fa_inverse-warp.nii.gz
 
-mv ${FA_ANT_PRE}Warp.nii.gz ${FA_ANT_PRE}warp.nii.gz
-mv ${FA_ANT_PRE}InverseWarp.nii.gz ${FA_ANT_PRE}invwarp.nii.gz
-mv ${FA_ANT_PRE}Affine.txt ${FA_ANT_PRE}affine.txt
+#mv ${FA_ANT_PRE}Warp.nii.gz ${FA_ANT_PRE}warp.nii.gz
+#mv ${FA_ANT_PRE}InverseWarp.nii.gz ${FA_ANT_PRE}invwarp.nii.gz
+#mv ${FA_ANT_PRE}Affine.txt ${FA_ANT_PRE}affine.txt
 
+#------------------------------------------------------------------------------
+# FA maps Registration on the SUB_STAT
+#------------------------------------------------------------------------------
 
+echo "Applying the warp..."
+
+WarpImageMultiTransform 3  \
+	${FA_MOVE} \
+	$OUT/fa_aligned.nii.gz \
+	Warp.nii.gz Affine.txt
