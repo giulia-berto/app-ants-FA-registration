1727abec0d4ffd67ae72bda4e6ff827466ff2f2b
gberto <gberto@fbk.eu>
2020-06-25 04:34:25 -0400

    update

---
+++ ants_fa_registration.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+FA_MOVE=$1
+MD_MOVE=$2
+RD_MOVE=$3
+AD_MOVE=$4
+template=FMRIB58_FA_1mm.nii.gz
+
+#------------------------------------------------------------------------------
+# ANTS Transformation of structural images based on FA
+#------------------------------------------------------------------------------
+
+# Warp Computation
+ANTS 3 -m CC[${template},${FA_MOVE},1,5] -t SyN[0.5] \
+    -r Gauss[2,0] -o -i 30x90x20 --use-Histogram-Matching
+
+mkdir warp
+cp 1Warp.nii.gz warp/warp.nii.gz
+cp 1InverseWarp.nii.gz warp/inverse-warp.nii.gz
+
+#------------------------------------------------------------------------------
+# tensor Registration to the template
+#------------------------------------------------------------------------------
+
+echo "Applying the warp..."
+mkdir tensor_aligned
+${OUT}=tensor_aligned
+
+WarpImageMultiTransform 3  \
+	${FA_MOVE} \
+	${OUT}/fa.nii.gz \
+	1Warp.nii.gz 1Affine.txt
+
+WarpImageMultiTransform 3  \
+	${MD_MOVE} \
+	${OUT}/md.nii.gz \
+	1Warp.nii.gz 1Affine.txt
+
+WarpImageMultiTransform 3  \
+	${RD_MOVE} \
+	${OUT}/rd.nii.gz \
+	1Warp.nii.gz 1Affine.txt
+
+WarpImageMultiTransform 3  \
+	${AD_MOVE} \
+	${OUT}/ad.nii.gz \
+	1Warp.nii.gz 1Affine.txt
